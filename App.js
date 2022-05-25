1eaf552145363dfc60f3cd4a19a6eedc3272b545
Talha <ranatalhaa01@gmail.com>
2022-05-25 16:11:23 +0500

    New Project

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <Text> Starting</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
