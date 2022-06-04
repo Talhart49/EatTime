f3f8b42d171c20f05e70ff0af9008aeae1a3d016
Talha <ranatalhaa01@gmail.com>
2022-06-04 22:13:18 +0500

    customizing the drawer

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,15 +1,28 @@
 import {View, Text} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import React from 'react';
-import HomeScreen from '../screens/HomeScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {DrawerActions} from '@react-navigation/native';
+import MainTabScreen from '../screens/MainTabScreen';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Settings from '../screens/Settings';
+import Orders from '../screens/Orders';
+import Recipes from '../screens/Recipes';
 
-const Stack = createStackNavigator();
+import React from 'react';
+
+const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <>
+      <Drawer.Navigator screenOptions={{headerTitleAlign: 'center'}}>
+        <Drawer.Screen name="Home" component={MainTabScreen} />
+        <Drawer.Screen name="Orders" component={Orders} />
+        <Drawer.Screen name="Recipes" component={Recipes} />
+
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
+    </>
   );
 };
 
