e72af9f2a6bb57efeaa59f30eddefabfacd06837
Talha <ranatalhaa01@gmail.com>
2022-06-04 23:38:10 +0500

    Customized Drawer

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -9,18 +9,23 @@ import Orders from '../screens/Orders';
 import Recipes from '../screens/Recipes';
 
 import React from 'react';
+import DrawerContent from '../screens/DrawerContent';
+import CartScreen from '../screens/CartScreen';
+import PaymentScreen from '../screens/PaymentScreen';
+import SupportScreen from '../screens/SupportScreen';
 
 const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
   return (
     <>
-      <Drawer.Navigator screenOptions={{headerTitleAlign: 'center'}}>
-        <Drawer.Screen name="Home" component={MainTabScreen} />
-        <Drawer.Screen name="Orders" component={Orders} />
-        <Drawer.Screen name="Recipes" component={Recipes} />
-
-        <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Navigator
+        screenOptions={{headerTitleAlign: 'center'}}
+        drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name=" " component={MainTabScreen} />
+        <Drawer.Screen name="Cart" component={CartScreen} />
+        <Drawer.Screen name="Payment" component={PaymentScreen} />
+        <Drawer.Screen name="Support" component={SupportScreen} />
       </Drawer.Navigator>
     </>
   );
---
+++ screens/DrawerContent.js
@@ -0,0 +1,153 @@
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {Avatar, Title, Caption, Drawer} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import {DrawerContentScrollView, DrawerItem} from '@react-navigation/drawer';
+
+const DrawerContent = props => {
+  return (
+    <View style={{flex: 1}}>
+      <DrawerContentScrollView {...props}>
+        <View style={styles.drawerContent}>
+          <View style={styles.userInfoSection}>
+            <View style={{flexDirection: 'row', marginTop: 15}}>
+              <Avatar.Image source={require('../assets/salad.png')} size={50} />
+              <View style={{marginLeft: 15, flexDirection: 'column'}}>
+                <Title style={styles.title}>Abdullah Sad</Title>
+                <Caption style={styles.caption}>sad247@gmail.com</Caption>
+              </View>
+            </View>
+            <Drawer.Section style={styles.drawerSection}>
+              <DrawerItem
+                icon={({color, size}) => (
+                  <Icon name="home-outline" color={color} size={size} />
+                )}
+                label="Home"
+                onPress={() => {
+                  props.navigation.navigate('Home');
+                }}
+              />
+              <DrawerItem
+                icon={({color, size}) => (
+                  <Icon name="basket-outline" color={color} size={size} />
+                )}
+                label="Orders"
+                onPress={() => {
+                  props.navigation.navigate('Orders');
+                }}
+              />
+              <DrawerItem
+                icon={({color, size}) => (
+                  <Icon name="food-outline" color={color} size={size} />
+                )}
+                label="Recipes"
+                onPress={() => {
+                  props.navigation.navigate('Recipes');
+                }}
+              />
+
+              <DrawerItem
+                icon={({color, size}) => (
+                  <Icon name="cart-check" color={color} size={size} />
+                )}
+                label="View My Cart"
+                onPress={() => {
+                  props.navigation.navigate('Cart');
+                }}
+              />
+
+              <DrawerItem
+                icon={({color, size}) => (
+                  <AntDesign name="creditcard" color={color} size={size} />
+                )}
+                label="Payment Methods"
+                onPress={() => {
+                  props.navigation.navigate('Payment');
+                }}
+              />
+
+              <DrawerItem
+                icon={({color, size}) => (
+                  <AntDesign name="setting" color={color} size={size} />
+                )}
+                label="Account Settings"
+                onPress={() => {
+                  props.navigation.navigate('Settings');
+                }}
+              />
+
+              <DrawerItem
+                icon={({color, size}) => (
+                  <Feather name="phone-call" color={color} size={size} />
+                )}
+                label="Support"
+                onPress={() => {
+                  props.navigation.navigate('Support');
+                }}
+              />
+              <DrawerItem
+                icon={({color, size}) => (
+                  <AntDesign name="logout" color={color} size={size} />
+                )}
+                label="Logout"
+                onPress={() => {
+                  props.navigation.navigate('Settings');
+                }}
+              />
+            </Drawer.Section>
+          </View>
+        </View>
+      </DrawerContentScrollView>
+    </View>
+  );
+};
+
+export default DrawerContent;
+
+const styles = StyleSheet.create({
+  drawerContent: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingLeft: 20,
+  },
+  title: {
+    fontSize: 16,
+    marginTop: 3,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  row: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  paragraph: {
+    fontWeight: 'bold',
+    marginRight: 3,
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+  bottomDrawnSection: {
+    marginTop: 15,
+    borderTopColor: '#f4f4f4',
+    borderTopWidth: 1,
+  },
+  preference: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+  },
+});
